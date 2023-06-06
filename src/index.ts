3675c71424a6a124271f3f04e374ccf11b08852e
mohamedsaiedd <mohamed00sd6@gmail.com>
2023-06-06 05:20:04 +0200

    add initializers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+let holder = document.querySelectorAll('.JSuspense-wraper .content');
+window.addEventListener("load", (event) => {
+    console.log('loaded');
+    holder.forEach((ele: any) => {
+        ele.classList.add('loaded')
+        ele.parentElement.classList.remove('JSuspense-wraper')
+    })
+});
